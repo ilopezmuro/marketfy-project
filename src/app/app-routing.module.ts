f0a76fa68ebdc2ebcf14ef9106cb5823a7dd8e71
Ivan_Maximiliano <ilopezmuro@deloitte.com>
2024-03-22 09:38:01 -0600

    Refactoring a few things, made navigation a separate component, made new components for product list and product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsListComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailsComponent,
+    NavigationBarComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
