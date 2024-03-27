641ad46957e0da87d33e09ef24162baffb034077
Ivan_Maximiliano <ilopezmuro@deloitte.com>
2024-03-27 13:47:38 -0600

    Order history component changed, now it calls the backend and properly shows the history

--- src/app/pages/order-history/order-history.component.ts
+++ src/app/pages/order-history/order-history.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SessionServiceService } from '../../services/session-service.service';
 
 @Component({
   selector: 'app-order-history',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './order-history.component.css'
 })
 export class OrderHistoryComponent {
+
+  orderHistoryDetails: any;
+
+  constructor(private http: HttpClient, private sessionService: SessionServiceService) { 
+
+    let userId = sessionService.getUserSession()['id'];
+
+    this.http.get<any[]>(`http://localhost:8080/shoppingcart/v2/products/${ userId }/order-details`)
+    .subscribe( (response) => {
+
+      const mappedOrderHistoryDetails: any[] = response.map( item => {
+
+        return {
+
+          order_id: item[0],
+          order_date: item[1],
+          product_name: item[2],
+          product_price: item[3]
+
+        }
+
+      });
+
+      this.orderHistoryDetails = mappedOrderHistoryDetails;
+
+    });
+
+  }
 
 }
