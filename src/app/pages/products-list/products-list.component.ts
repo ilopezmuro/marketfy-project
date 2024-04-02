d3bcc34525ef0057ef71f2253d6e9cf0f8fc7aa9
Ivan_Maximiliano <ilopezmuro@deloitte.com>
2024-04-02 11:52:03 -0600

    Added add favorite button, for now it only shows if user has it favorited or not

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import type { product } from '../../interfaces/product-interface';
+import { SessionServiceService } from '../../services/session-service.service';
 
 @Component({
   selector: 'app-products-list',
@@ -11,9 +12,21 @@ export class ProductsListComponent {
 
   products: product[] = [];
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private sessionService: SessionServiceService){
 
     this.getProducts();
+
+  }
+
+  getIfFavorite(id: number){
+    let user_id = this.sessionService.userLoggedId;
+
+    this.http.get<boolean>(`http://localhost:8080/shoppingcart/v2/wishlist/exists-user-favorite/${user_id}/${id}`)
+    .subscribe( (exists) => {
+
+      console.log(exists);
+
+    });
 
   }
 
