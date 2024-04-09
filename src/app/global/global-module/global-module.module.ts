20c381dbcfbbbd520d6ca0ec67d88e1bdb86266f
Ivan_Maximiliano <ilopezmuro@deloitte.com>
2024-04-09 15:09:36 -0600

    Adjusted global module so the navigation component can route properly

--- src/app/global/global-module/global-module.module.ts
+++ src/app/global/global-module/global-module.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   declarations: [NavigationBarComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    AppRoutingModule
   ],
   exports: [NavigationBarComponent]
 })
